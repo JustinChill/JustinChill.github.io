eb11a8aaae830b478c6acb44db7098f95096b7ce
Justin Hill <justinrichardhill@gmail.com>
2025-03-16 14:39:37 -0400

    Growing Up is hard to do

--- js/growup.js
+++ js/growup.js
@@ -9,42 +9,50 @@ function growUp({
     ...options,
     render(index, scales, values, dimensions, context, next) {
       const g = next(index, scales, values, dimensions, context);
-      const R = g.querySelectorAll("rect");
-      const T = Float64Array.from(R, () => 1);
-      const Y = Float64Array.from(R, (r) => r.getAttribute("y"));
-      const H = Float64Array.from(R, (r) => r.getAttribute("height"));
+      const rects = Array.from(g.querySelectorAll("rect"));
 
       g.style.opacity = "0";
       g.style.transition = "opacity 0.8s ease-out";
+      
+      const rectData = rects.map(r => ({
+        rect: r,
+        originalY: parseFloat(r.getAttribute("y")),
+        originalHeight: parseFloat(r.getAttribute("height"))
+      }));
 
       function startAnimation() {
         g.style.opacity ="1"; // Fade in
         const start = performance.now();
 
         (function tick(now) {
-          let tt = false;
-          for (let i = 0, n = R.length; i < n; ++i) {
-            const t = ease(Math.max(0, Math.min(1, (now - start - (delay + stagger * i / n)) / duration)));
-            if (t < 1) tt = true;
-            if (T[i] === t) continue; // don’t touch if unchanged
-            R[i].setAttribute("y", Y[i] + H[i] * (1 - t));
-            R[i].setAttribute("height", H[i] * t);
-            T[i] = t;
+          const elapsed = now - start;
+          rectData.forEach(({ rect, originalY, originalHeight }, i) => {
+            const t = ease(Math.min(1, Math.max(0, (elapsed - delay - (stagger * i / rects.length)) / duration)));
+            rect.setAttribute("y", originalY + originalHeight * (1 -t));
+            rect.setAttribute("height", originalHeight * t);
+          });
+            
+          if (elapsed < duration + delay + stagger){
+            requestAnimationFrame(tick);
           }
-          if (tt) requestAnimationFrame(tick);
         })(start);
       }
+      
+      if('IntersectionObserver' in window) {
+        // Intersection Observer to detect when in viewport
+        const observer = new IntersectionObserver((entries, observer) => {
+          entries.forEach(entry => {
+            if(entry.isIntersecting) {
+              startAnimation();
+              observer.unobserve(entry.target);
+            }
+          });
+        }, {threshold: 0.3 });
+        observer.observe(g);
+      } else {
+          startAnimation();
+      }
 
-      // Intersection Observer to detect when in viewport
-      const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-          if(entry.isIntersecting) {
-            startAnimation();
-            observer.unobserve(entry.target);
-          }
-        });
-      }, {threshold: 0.3 });
-      observer.observe(g);
       return g;
     }
   };
